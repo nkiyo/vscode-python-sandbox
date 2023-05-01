1329646c9d05d63473cc12f8763210600b46bc38
nkiyo <nakahara.kiyoshi@gmail.com>
2023-05-01 19:57:01 +0900

    lanch、attachによるデバッグに成功した時点

--- sample.py
+++ sample.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    print("hoge")
     result = sum.sum(1, 2)
+    print(f"### result is {result} ###")
     return f"<p>Hello, World! {result}</p>"
+
+
+# app.run()があることで、普通のpythonコードとして起動可能
+# デバッガと組み合わせるときは、こちらの起動方法がやりやすそう
+if __name__ == "__main__":
+    app.run()
